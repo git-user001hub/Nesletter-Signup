b2bfbdbc157704615d02cea8268e3556b8ef1b76
git-user123hub <149748145+git-user123hub@users.noreply.github.com>
2023-11-05 10:56:14 +0000

    Update app.js api key

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/", function(req, res){
 
     const options = {
         method: "POST",
-        auth: "vbar1:baaab0770637717bffd5bd5349cc81aba-us21"
+        auth: "vbar1:aaab0770637717bffd5bd5349cc81aba-us21"
     }
 
     const request = https.request(url, options, function(response){
